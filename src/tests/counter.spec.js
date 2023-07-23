e74a6e5b244ed6b83e2192d43c9286173e2b82fa
Cloud <BetaPhoenixSNSD@gmail.com>
2023-07-23 15:30:54 +0800

    feat.yaml setting

--- src/tests/counter.spec.js
+++ src/tests/counter.spec.js
@@ -5,5 +5,5 @@ test('初始標題文字 = 當前點擊次數0', () => {
   const wrapper = shallowMount(Counter);
 
   const title = wrapper.find('[data-testid="currentCounter"]');
-  expect(title.text()).toBe('當前點擊次數10');
+  expect(title.text()).toBe('當前點擊次數0');
 });
